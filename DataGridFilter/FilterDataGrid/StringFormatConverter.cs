583cee053737611bc74234877826dabf37ed6047
Gilles Macabies <gmacabies@phareway.com>
2021-01-31 15:07:00 +0100

    stringForamt

--- DataGridFilter/FilterDataGrid/StringFormatConverter.cs
+++ DataGridFilter/FilterDataGrid/StringFormatConverter.cs
@@ -22,7 +22,7 @@ namespace FilterDataGrid
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values[0] == DependencyProperty.UnsetValue || string.IsNullOrEmpty(values[0].ToString())) return "";
+            if (values[0] == DependencyProperty.UnsetValue || string.IsNullOrEmpty(values[0]?.ToString())) return "";
 
             var stringFormat = values[0].ToString();
 
