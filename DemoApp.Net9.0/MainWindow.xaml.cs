57eeb080688aff6472f8321655889b4346fb3166
Daniel LUCCA <daniel.lucca@siemens-energy.com>
2025-05-22 09:46:36 +0200

    Add support for .NET 9.0 and new project structure
    Updated project files to target .NET 9.0 and added new projects for DemoApp.Net8.0 and DemoApp.Net9.0.
    Refactored Employe.cs and RandomGenerator.cs to align with the new structure.

---
+++ DemoApp.Net9.0/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿#region (c) 2022 Gilles Macabies All right reserved
+
+// Author     : Gilles Macabies
+// Solution   : FilterDataGrid
+// Projet     : DemoApp.Net9.0
+// File       : MainWindow.xaml.cs
+// Created    : 04/12/2022
+//
+
+#endregion
+
+using SharedModelView.ModelView;
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace DemoApp.Net9._0
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Public Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+#if DEBUG
+            AppDomain.CurrentDomain.FirstChanceException += (source, e) =>
+            {
+                Debug.WriteLine("FirstChanceException event raised in " +
+                                $"{AppDomain.CurrentDomain.FriendlyName}: {e.Exception.Message} {source}");
+            };
+#endif
+
+            DataContext = new ModelView();
+        }
+
+        #endregion Public Constructors
+    }
+}
